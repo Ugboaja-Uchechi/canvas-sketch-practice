2da2469a0088c589e5a58d7d3b41135ccfadc022
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-06-08 00:22:15 +0100

    Build a clock

--- sketch/transform.js
+++ sketch/transform.js
@@ -1,7 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080]
 };
 
 const degToRad = (deg) => {
@@ -13,13 +13,17 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     context.fillStyle = 'orange';
-    const x = width * 0.5;
-    const y = height * 0.5;
+    // we renamed x and y to cx and cy to be the center of the circle
+    const cx = width * 0.5;
+    const cy = height * 0.5;
 
     // 30% of the canvas width and height is used
     const w = width * 0.01;
     const h = height * 0.1;
-    
+
+    // we declared x and y with let so we can modify the values in the loop
+    let x, y;
+
     // context.save();
     // context.translate(x,y);
     // // rotate is in radians not degree
@@ -46,11 +50,28 @@ const sketch = () => {
     // context.fill();
     // context.restore();
 
-    context.translate(x,y);
-    context.rotate(degToRad(45));
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
+    const num = 12;
+    const radius = width * 0.3;
+
+    for (let i = 0; i < num; i++) {
+      // change the angle of the location by the index of the loop
+      const slice = degToRad(360 / num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
+
+
   };
 };
 
