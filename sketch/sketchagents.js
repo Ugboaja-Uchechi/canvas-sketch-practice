a968c4b98c672afacf2e29f0f076d352434f7e90
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-07-09 00:11:29 +0100

    Animate agents

--- sketch/sketchagents.js
+++ sketch/sketchagents.js
@@ -1,9 +1,17 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
+// to animate it needs to be specified in the settings
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
+
+// another way to animate without using canvas
+// const animate = () => {
+//   console.log('animate');
+//   requestAnimationFrame(animate); 
+// }
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -24,7 +32,9 @@ const sketch = ({ context, width, height }) => {
     // const agentB = new Agent(300, 700, 10);
 
     agents.forEach(agent => {
+      agent.update()
       agent.draw(context);
+      agent.bounce(width, height);
     })
 
   };
@@ -32,7 +42,7 @@ const sketch = ({ context, width, height }) => {
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
@@ -42,14 +52,36 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(2, 12);
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      // invert the velocity
+      this.vel.x *= -1;
+    }
+
+    if (this.pos.y <= 0 || this.pos.y >= height) {
+      // invert the velocity
+      this.vel.y *= -1;
+    }
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
 
   draw(context) {
-    context.fillStyle = 'black';
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+    context.lineWidth = 2.5;
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+    context.restore();
   }
 }
